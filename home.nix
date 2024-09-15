c348f0b3f5b1c5610112249f5fad842135ca130e
kaleb <kalebroyolson+github@gmail.com>
2024-09-15 12:57:23 -0500

    dynamic username

--- home.nix
+++ home.nix
@@ -1,10 +1,13 @@
 { config, pkgs, ... }:
 
+let 
+  username = builtins.getEnv "USER";
+in
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "kaleb";
-  home.homeDirectory = "/home/kaleb";
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
