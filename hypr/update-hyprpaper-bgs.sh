c4c662c47ebbb24e2afafa559292e9ceaa43a3c7
Kaleb Olson <kalebroyolson+github@gmail.com>
2024-09-19 22:16:37 -0500

    customize waybar

---
+++ hypr/update-hyprpaper-bgs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo help
+echo home: $HOME
+echo 1: $1
+if [ -z '$1' ]; then
+  hd=$HOME
+else 
+  hd=$1
+fi
+
+directory=$hd/Pictures/hyprpaper-rotation
+monitors=$(hyprctl monitors | grep Monitor | awk '{print $2};')
+
+
+if [ -d "$directory" ]; then
+
+  hyprctl hyprpaper unload all
+
+  while IFS= read -r monitor; do
+    echo $monitor
+    bg_filename=$(ls $directory | shuf -n 1)
+    bg_path=$directory/$bg_filename
+
+    hyprctl hyprpaper preload $bg_path
+    hyprctl hyprpaper wallpaper "$monitor, $bg_path"
+  done <<< "$monitors"
+fi
+
